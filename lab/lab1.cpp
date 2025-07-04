1c6245b058be744ee35b241e4f6342dce42b5031
nara-bodee <narabodeeboonlead@gmail.com>
2025-07-04 15:53:06 +0700

    lab

--- lab/lab1.cpp
+++ lab/lab1.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char Name[50] ;
-    int  Age = 0 ;
-    printf( "Enter your name: " ) 
-    scanf( "%s", Name ) ;
-    printf( "Enter your age: " ) ;
-    scanf( "%d", Age ) ;
-    print( "- - - - - -\n" ) ;
-    printf( "Hello %s \n", ___ ) ; 
-    printf( "Age = %d\n", ___ ) ; 
-    
-}//end main function
+    char Name[50];
+    int Age = 0;
+
+    printf("Enter your name: ");
+    scanf("%s", Name);
+
+    printf("Enter your age: ");
+    scanf("%d", &Age); 
+
+    printf("- - - - - -\n");
+    printf("Hello %s\n", Name); 
+    printf("Age = %d\n", Age);
+
+    return 0;
+}
