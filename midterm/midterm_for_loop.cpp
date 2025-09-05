96a2fccfd3147afb016642c213c4801537965663
nara-bodee <narabodeeboonlead@gmail.com>
2025-09-05 12:52:54 +0700

    midterm

--- midterm/midterm_for_loop.cpp
+++ midterm/midterm_for_loop.cpp
@@ -2,20 +2,21 @@
 
 
 int main() {
-    int N, i, j, prime;
+    int N, i, j;
     printf("Enter number : ");
     scanf("%d", &N);
 
-    for (i = N; i >= 2; i--) {
+    for (i = N; i >= 2; i--) { // วนจาก N ลงไปถึง 2
   
         for (j = 2; j < i; j++) { // ลูปตรวจสอบตัวหาร
             if (i % j == 0) { // ถ้า i หาร j ลงตัว
                 break; // ออกจากลูปทันที
-            }
-        }
+            }//end if
+        }//end for
+        
         if (j == i) { // ถ้า j วิ่งมาถึง i ได้ แสดงว่าไม่มีตัวหาร
         printf("%d ", i);
-        }
-    }
+        }//end if
+    }//end for
     return 0;
-}
+}//end main
