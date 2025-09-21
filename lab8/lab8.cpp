44e83376b92bb393b24cc39bd705a67f5d82fd9f
nara-bodee <narabodeeboonlead@gmail.com>
2025-09-21 08:38:11 +0700

    lab10

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -1,31 +1,31 @@
 #include<stdio.h>
 int main() {
-    int position, year, project;
-    int base_salary[3] = {20000, 35000, 50000};
-    int salary;
-    float bonus, specialbonus, netSalary;
+    int position, year, project; //การกำหนดตัวแปร
+    int base_salary[3] = {20000, 35000, 50000}; //เงินเดือนพื้นฐานของแต่ละตำแหน่ง (0=junior, 1=senior, 2=manager)
+    int salary; //เงินเดือนพื้นฐาน
+    float bonus, specialbonus, netSalary; //โบนัส, โบนัสพิเศษ, เงินเดือนสุทธิ
 
-    printf("Position: ");
+    printf("Position: "); //รับค่าตำแหน่ง
     scanf("%d", &position);
-    printf("Years of Experience: ");
+    printf("Years of Experience: "); //รับค่าปีที่ทำงาน    
     scanf("%d", &year);
-    printf("Number of Projects Completed this Year: ");
+    printf("Number of Projects Completed this Year: "); //รับค่าจำนวนโปรเจคที่ทำได้ในปีนี้
     scanf("%d", &project);
 
-    salary = base_salary[position - 1];
-    if (year < 1)
+    salary = base_salary[position - 1]; //กำหนดเงินเดือนพื้นฐานตามตำแหน่ง สมมติถ้า position ไม่ลบ 1 จะเป็นการเข้าถึง index จะไม่ถูกต้อง
+    if (year < 1) //กำหนดโบนัสตามปีที่ทำงาน
         bonus = 0;
     else if (year >= 1 && year <= 3)
-        bonus = salary * 0.10;
+        bonus = salary * 0.10; //10% ของเงินเดือน
     else if (year >= 4 && year <= 5)
         bonus = salary * 0.15;
     else
         bonus = salary * 0.20;
 
     if (project > 5)
-        specialbonus = salary * 0.05;
+        specialbonus = salary * 0.05; //ถ้าทำโปรเจคเกิน 5 ได้โบนัสพิเศษ 5%
 
-    netSalary = salary + bonus + specialbonus;
+    netSalary = salary + bonus + specialbonus; //คำนวณเงินเดือนสุทธิ
 
     printf("Base Salary: %d THB\n", salary);
     printf("Experience Bonus: %.0f THB\n", bonus);
