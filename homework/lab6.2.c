3af3d431c291f261ee65dc63297919c3f2359323
nara-bodee <narabodeeboonlead@gmail.com>
2025-10-11 12:13:33 +0700

    lab6.2

---
+++ homework/lab6.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("Enter value: ");
+    scanf("%d", &N);
+
+    printf("Series: ");
+    if (N % 2 == 1) {
+
+        for (int i = 1; i <= N; i += 2) {
+            printf("%d ", i);
+        }
+    } else {
+
+        for (int i = N; i >= 0; i -= 2) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
